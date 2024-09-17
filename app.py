40aecce89b781da372d1d4be960b5304dbe21ccf
PhilosophyWithJosh <joshmendiola@icloud.com>
2024-09-17 12:20:19 -0500

    keep it simple, stupid

--- app.py
+++ app.py
@@ -1,39 +1,11 @@
-import os
-from flask import Flask, send_from_directory, abort
-import logging
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.DEBUG)
-
-mojorepair_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'frontend', 'MoJoRepair'))
 
 
 @app.route('/')
-def home():
-    app.logger.info(f"Serving home page from {mojorepair_dir}/index.html")
-    return send_from_directory(mojorepair_dir, 'index.html')
-
-
-@app.route('/<path:filename>')
-def serve_static(filename):
-    app.logger.info(f"Request for: /{filename}")
-
-    if filename in ['about', 'services', 'login', 'register']:
-        file_path = os.path.join(mojorepair_dir, 'html', f'{filename}.html')
-        app.logger.info(f"Attempting to serve: {file_path}")
-        if os.path.isfile(file_path):
-            return send_from_directory(os.path.join(mojorepair_dir, 'html'), f'{filename}.html')
-        else:
-            app.logger.error(f"File not found: {file_path}")
-            abort(404)
-    else:
-        file_path = os.path.join(mojorepair_dir, filename)
-        app.logger.info(f"Attempting to serve: {file_path}")
-        if os.path.isfile(file_path):
-            return send_from_directory(mojorepair_dir, filename)
-        else:
-            app.logger.error(f"File not found: {file_path}")
-            abort(404)
+def api_home():
+    return jsonify({"message": "Welcome to the MoJoRepair API"})
 
 
 if __name__ == '__main__':
