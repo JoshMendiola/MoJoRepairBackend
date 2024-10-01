298cf08941079c7a489d8dc479b16d318c763a66
PhilosophyWithJosh <joshmendiola@icloud.com>
2024-09-30 19:34:03 -0500

    fix database url

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://rootdbuser:{os.environ.get('MYSQL_PASSWORD')}@mysql:3306/mojorepairdb"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', os.urandom(24))
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
