7bef2157f1e9a726103afbee2dee8f93c57af0ef
PhilosophyWithJosh <joshmendiola@icloud.com>
2024-09-30 23:27:39 -0500

    candh ccane

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -e
 
-host="mysql"
-port="3306"
+host="$1"
+shift
+port="$1"
+shift
 cmd="$@"
 
 until nc -z $host $port; do
-  >&2 echo "MySQL is unavailable - sleeping"
+  >&2 echo "$host:$port is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "MySQL is up - executing command"
+>&2 echo "$host:$port is up - executing command"
 exec $cmd
